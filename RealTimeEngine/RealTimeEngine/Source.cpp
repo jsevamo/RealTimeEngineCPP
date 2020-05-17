9dd2abd92fb367128640abb54b9bae8e2770deb2
jsevamo <j_sevamo@hotmail.com>
2020-05-17 16:15:19 -0500

    Created shader program to link both Vertex and Fragment Shaders

--- RealTimeEngine/RealTimeEngine/Source.cpp
+++ RealTimeEngine/RealTimeEngine/Source.cpp
@@ -15,14 +15,14 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
-"void main(\n"
+"void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"void main(\n"
+"void main()\n"
 "{\n"
 "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 "}\n\0";
@@ -101,9 +101,23 @@ int main()
 		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
-	
 
-	
+	//Linking Shaders -----------------------------------------------------------------------------
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	//Check for compiler errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
 	//Vertex Input --------------------------------------------------------------------------------
 
 	float vertices[] = {
